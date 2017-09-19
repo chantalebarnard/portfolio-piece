73ee2a79f9d4dc87b8caaa97c0db8f1953810350
chantalebarnard <barnardchantale@gmail.com>
2017-09-19 18:00:59 -0400

    again

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,21 @@
-const ohHey = "Hello World";
+$(function(){
 
-console.log(ohHey);
+	console.log("asadasdasd");
+	$('.arrowContainer').click(function(e){
+		$('html, body').animate({
+			scrollTop: $('#nav').offset().top
+		}, 
+		1500);
+	});
+	$('.scroll').click(function(e){
+		var linkHref = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top
+		},
+		1500);
+		e.preventDefault();
+	});
+	
+	new WOW().init();
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,32 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-// a task to compile our sass
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/styles'))
 });
-
-// task to compile our javascript
 gulp.task('scripts', () => {
-	return gulp.src('./dev//main.js')
+	return gulp.src('./dev/scripts/main.js')
 		.pipe(babel({
-			presets: ['es2015', 'es2016']
+			presets: ['es2015']
 		}))
-		.pipe(gulp.dest('./public/scripts'))
+		.pipe(gulp.dest('./public/scripts'));
 });
 
-// a tas to watch all of my othr tasks 
-gulp.task('watch', () => {
-	gulp.watch('./dev/styles/**/*.scss', ['styles'])
-	gulp.watch('./dev/scripts/main.js', ['scripts'])
-})
-gulp.task('default', ['styles', 'scripts', 'watch']);
+
+gulp.task('browser-sync', () => {
+	browserSync.init({
+		server: '.'
+	});
+});
+
+gulp.task('default', ['styles', 'browser-sync', 'scripts'], () => {
+	gulp.watch('./dev/styles/**/*.scss', ['styles']);
+	gulp.watch('./dev/scripts/main.js'), ['scripts'];
+ 	gulp.watch('*.html', reload);
+});
